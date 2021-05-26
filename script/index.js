f1cab598dfd8cdbdedee1da20914b94206ec61b1
takker21 <77918435+takker21@users.noreply.github.com>
2021-05-26 15:11:46 +0300

    First Commit

---
+++ script/index.js
@@ -0,0 +1,30 @@
+const popup = document.querySelector('.popup');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupExitButton = popup.querySelector('.popup__exit-button');
+const formElement = document.querySelector('.popup__form');
+const popupFieldName = popup.querySelector('.popup__field_type_name');
+const popupFieldDescription = popup.querySelector('.popup__field_type_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupFieldName.value;
+  profileDescription.textContent = popupFieldDescription.value;
+  popupToggle();
+}
+
+profileEditButton.addEventListener('click', popupOpen);
+popupExitButton.addEventListener('click', popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
